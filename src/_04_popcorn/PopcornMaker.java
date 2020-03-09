fed2e3f7454d056f19b649d2f0021fe9408e3ae6
emma-l-myoosik <60460092+emma-l-myoosik@users.noreply.github.com>
2020-03-09 10:31:15 -0700

    PopcornMaker

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -1,11 +1,11 @@
 package _04_popcorn;
 
 public class PopcornMaker {
+	static String flavor;
 	public static void main(String[] args) {
 		Microwave microwave = new Microwave();
-
 		Popcorn popcorn = new Popcorn("butter");
-		
+		microwave.setTime(9);
 		
 		
 	}
